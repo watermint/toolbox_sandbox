b1ce01dccf3f7b6f95b72698d0abdf1c97c677ce
Takayuki Okazaki <watermint@users.noreply.github.com>
2020-11-18 09:51:29 +0900

    Release:2020-11-18T09:51:29+09:00

--- toolbox.rb
+++ toolbox.rb
@@ -2,7 +2,7 @@ class Toolbox < Formula
   desc "CLI tools for Dropbox and Dropbox Business"
   homepage "https://github.com/watermint/toolbox"
   url "https://raw.githubusercontent.com/watermint/toolbox/master/README.md"
-  sha256 "2670a389d286fe1f0b728b308fa12f6d42e54417acac7b220d49fbf1b92b9437"
+  sha256 "12bdf6a8be7b55265f21b411c6e9a87209f3c13308d5ed82de063f5b617cbe8f"
 
   def install
     bin.install "tbx"
