b144720580b706c975a6bc4b69c53a7d11eae299
Takayuki Okazaki <watermint@users.noreply.github.com>
2022-07-27 12:04:18 +0900

    Release:2022-07-27T12:04:17+09:00

--- toolbox.rb
+++ toolbox.rb
@@ -2,7 +2,7 @@ class Toolbox < Formula
   desc "The multi-purpose utility command-line tool for web services including Dropbox, Dropbox Business, Google, GitHub, etc."
   homepage "https://github.com/watermint/toolbox"
   url "https://raw.githubusercontent.com/watermint/toolbox/master/README.md"
-  sha256 "813ee481168c24a282c9b8b1cd000a8ef7a261a637419b6232ba5f8c25f04ec2"
+  sha256 "02cbe87d6ffbcff6329fa6ebe1f33afae4334c21f0bf23569882fda43bc1c7cd"
 
   def install
     bin.install "tbx"
