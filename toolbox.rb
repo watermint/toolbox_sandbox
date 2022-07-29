2e8ffe6a5d513d2bae6514488ed0a80beed5c20b
Takayuki Okazaki <watermint@users.noreply.github.com>
2022-07-29 17:57:39 +0900

    Release:2022-07-29T17:57:38+09:00

--- toolbox.rb
+++ toolbox.rb
@@ -1,19 +1,26 @@
 class Toolbox < Formula
   desc "The multi-purpose utility command-line tool for web services including Dropbox, Dropbox Business, Google, GitHub, etc."
   homepage "https://github.com/watermint/toolbox"
-  version "105.0.0-dev+20220728T053715Z"
+  version "107.0.0-dev+20220729T085738Z"
   license "MIT"
 
   on_linux do
-    url    "https://raw.githubusercontent.com/watermint/toolbox/master/README.md"
-    sha256 "b22b7cf084d210631bdaf875b3b83e228e4c274228b04f0af6df185007a09a62"
+    on_arm do
+      url    "https://raw.githubusercontent.com/watermint/toolbox/master/SECURITY_AND_PRIVACY.md"
+      sha256 "04b20191d728e828e30ba3df5668ba4e456608be13da95f13d94efc7c5182ab9"
+    end
+    on_intel do
+      url    "https://raw.githubusercontent.com/watermint/toolbox/master/README.md"
+      sha256 "b22b7cf084d210631bdaf875b3b83e228e4c274228b04f0af6df185007a09a62"
+    end
   end
 
   on_macos do
     on_arm do
       url    "https://raw.githubusercontent.com/watermint/toolbox/master/LICENSE.md"
       sha256 "ba72adc6998630b98723333b20d42da584c9f9fc32d9c83d1718605b458d982d"
-    else
+    end
+    on_intel do
       url    "https://raw.githubusercontent.com/watermint/toolbox/master/CONTRIBUTING.md"
       sha256 "ad1b52a2734c5de5fb23ed70660a8cd134464f34afd63f1f637c648ee5419ec2"
     end
