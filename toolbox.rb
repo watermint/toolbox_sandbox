09d1ee0a0f1139ff4d9ea1b13a061e4c8930305f
Takayuki Okazaki <watermint@users.noreply.github.com>
2024-01-19 19:41:29 +0900

    Release:2024-01-19T19:41:28+09:00

--- toolbox.rb
+++ toolbox.rb
@@ -1,7 +1,7 @@
 class Toolbox < Formula
   desc "The multi-purpose utility command-line tool for web services including Dropbox, Dropbox Business, Google, GitHub, etc."
   homepage "https://github.com/watermint/toolbox"
-  version "127.0.0-dev+20240115T074647Z"
+  version "128.0.0-dev+20240119T104128Z"
   license "MIT"
 
   on_linux do
@@ -11,7 +11,7 @@ class Toolbox < Formula
     end
     on_intel do
       url    "https://raw.githubusercontent.com/watermint/toolbox/master/README.md"
-      sha256 "8fc51bd263628ee934e57fe52c4edeef3421a44a3f2ec8afe69e1cab19c8e642"
+      sha256 "3d0700763b2db7c13e2f2ad81d3906c5a478d8b7ae9f9e53049343ab7a9e757d"
     end
   end
 
