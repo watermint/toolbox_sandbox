550d38016fd6361934b2a48af4672185ee46cfd8
Takayuki Okazaki <watermint@users.noreply.github.com>
2024-08-24 00:05:18 +0900

    Release:2024-08-24T00:05:17+09:00

--- toolbox.rb
+++ toolbox.rb
@@ -1,7 +1,7 @@
 class Toolbox < Formula
   desc "The multi-purpose utility command-line tool for web services including Dropbox, Dropbox Business, Google, GitHub, etc."
   homepage "https://github.com/watermint/toolbox"
-  version "136.0.0-dev+20240823T143852Z"
+  version "136.0.0-dev+20240823T150517Z"
   license "MIT"
 
   on_linux do
